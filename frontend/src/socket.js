2d3a42101a2938021da3c28d5a5d615e81b07fc7
Talha Kapadiya <tkap1234@gmail.com>
2026-03-29 15:06:29 -0400

    Fix socket URL for production

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
 // Single shared socket instance
-export const socket = io("http://localhost:3001", {
+const URL = import.meta.env.DEV ? "http://localhost:3001" : window.location.origin;
+
+export const socket = io(URL, {
   autoConnect: true,
 });
